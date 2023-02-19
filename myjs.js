f55f6f7f92e9fd0c40c8bfeceae224e679e5601f
ifat2023 <ifat.elgart@gmail.com>
2023-02-19 20:55:00 +0200

    first commit

--- myjs.js
+++ myjs.js
@@ -212,7 +212,9 @@ function ex14() {
 //Exs.15
 function ex15() {
   let funds = prompt("Please specify the number of funds in the train");
-  let time = prompt( "Please specify the time of day ? morning, noon or evening");
+  let time = prompt(
+    "Please specify the time of day ? morning, noon or evening"
+  );
   let salary = prompt("The driver's salary");
 
   const price = 6.6;
@@ -225,47 +227,220 @@ function ex15() {
   let total = 0;
 
   if (time == "morning") {
-    total = (morning - ( tax  * morning)) - salary;
+    total = morning - tax * morning - salary;
   } else if (time == "noon") {
-    total = (noon - ( tax  * noon)) - salary;
+    total = noon - tax * noon - salary;
   } else if (time == "evening") {
-    total = (evening - ( tax  * evening)) - salary;
+    total = evening - tax * evening - salary;
   }
 
-  document.write( " Net profit from operating the train line in the morning " + total );
+  document.write(
+    " Net profit from operating the train line in the morning " + total
+  );
 }
-
 
 function star() {
   let n = prompt("Please enter your number ");
 
-  for (let i=0; i < n; i++){
-    document.write(  "*" );
-  } 
+  for (let i = 0; i < n; i++) {
+    document.write("*");
+  }
 }
-
 
 function assembly() {
-  let n = Number( prompt("Please enter your number "));
-let total=1;
-  for (let i=1; i<=n; i++){
-    total=total*i;
-   
-  } 
-  document.write( n + "! = " +  total );
+  let n = Number(prompt("Please enter your number "));
+  let total = 1;
+  for (let i = 1; i <= n; i++) {
+    total = total * i;
+  }
+  document.write(n + "! = " + total);
 }
 
+//Exs. second home-work 1-12
 
-function largerNumber() {
+//Exs.1
 
- const num = [a,b];
-let i = 0
-if (a > i && b < a > i) {
-  document.write("the larger number is " + "a")
+function ex101() {
+  let y = Number(prompt("Please enter first number "));
+  let z = Number(prompt("Please enter second number"));
+
+  if (y > z) {
+    document.write("The larger number is " + y);
+  } else {
+    document.write("The larger number is " + z);
+  }
 }
-else{
-  document.write("the larger number is " + "b")
+
+//Exs.2
+function ex102() {
+  let e = Number(prompt("enter e"));
+  let f = Number(prompt("enter f"));
+  let g = Number(prompt("enter g"));
+
+  let counter = 0;
+
+  if (e < 0) {
+    counter++;
+  }
+  if (f < 0) {
+    counter++;
+  }
+  if (g < 0) {
+    counter++;
+  }
+
+  if (counter % 2 == 0) {
+    document.write("The sign is  + ");
+  } else {
+    document.write("The sign is - ");
+  }
 }
 
-document.write("the larger number is " + "b")
+//Exs.3
+
+function ex103() {
+  let h = 0
+  let i = -1
+  let j = 4
+
+  if (j > h && j > i) {
+    document.write(j + "," + h + "," + i);
+  } else document.write(h + "," + i + "," + j);
+}
+
+//Exs.4
+
+function ex104() {
+  let num1 = Number(prompt("Please enter first number"));
+  let num2 = Number(prompt("Please enter second number"));
+  let num3 = Number(prompt("Please enter Third number"));
+  let num4 = Number(prompt("Please enter Fourth number"));
+  let num5 = Number(prompt("Please enter Fifth number"));
+
+  if (num1 > num2 && num1 > num3 && num1 > num4 && num1 > num5) {
+    document.write("The larger number is " + num1);
+  } else if (num2 > num1 && num2 > num3 && num2 > num4 && num2 > num5) {
+    document.write("The larger number is " + num2);
+  } else if (num3 > num1 && num3 > num2 && num3 > num4 && num3 > num5)
+    document.write("The larger number is " + num3);
+  else if (num4 > num1 && num4 > num2 && num4 > num3 && num4 > num5)
+    document.write("The larger number is " + num4);
+  else if (num5 > num1 && num5 > num2 && num5 > num3 && num5 > num4)
+    document.write("The larger number is " + num5);
+}
+
+//Exs.5
+
+function ex105() {
+  let n = 15
+  for (let i = 0; i <= n; i++) {
+    if (i % 2 == 0) {
+      document.write(i + "  is even " + "</br>");
+    } else {
+      document.write(i + "  is odd " + "</br>");
+    }
+  }
+}
+
+//Exs.6
+
+function ex106() {
+  let David = 80;
+  let vinoth = 77;
+  let Divya = 88;
+  let Lshitha = 95;
+  let Thomas = 68;
+
+  let sum = David + vinoth + Divya + Lshitha + Thomas;
+  average = sum / 5;
+
+  if (average < 60) {
+    document.write(" The average is " + average + ", grade F");
+  } else if (average < 70) {
+    document.write(" The average is " + average + ", grade D");
+  } else if (average < 80) {
+    document.write(" The average is " + average + ", grade C");
+  } else if (average < 90) {
+    document.write(" The average is " + average + ", grade B");
+  } else if (average < 100) {
+    document.write(" The average is " + average + ", grade A");
+  }
+}
+
+//Exs.7
+
+function ex107() {
+  let n = 100;
+
+  for (let i = 1; i <= n; i++)
+  {
+    if (i % 3 === 0 && i % 5 === 0) {
+      document.writeln(i + " FizzBuzz " + "</br>" );
+    } else if (i % 3 === 0) {
+      document.writeln(i + " Fizz " + "</br>" );
+    } else if (i % 5 === 0 ) {
+      document.writeln(i + " Buzz " + "</br>" );
+    } else {
+      document.writeln(i + "</br>");
+    }
+  }
+}
+
+//Exs.10
+function ex110() {
+
+for (let i = 1; i <6; i++) {
+  for (let j = 0; j <i; j++) {
+    document.write("* ");
+  }
+  document.write("<br/>");
+
+}
+}
+
+//Exs.11
+
+function ex111() {
+  let num1 = Number(prompt("Please enter first number"));
+  let num2 = Number(prompt("Please enter second number"));
+
+  let gcd = getGcd(num1, num2);
+  document.write(gcd);
+}
+
+function getGcd(num1,num2){
+  let lowest = 0;
+  if (num1 < num2 )
+{
+  lowest = num1
+}
+else {
+  lowest = num2
+}
+ for (let i = lowest; i > 0; i--) {
+let result = isGcd(num1, num2, i);
+if (result == true) {
+return i;
+ }
+}
+
+return 1;
+}
+
+function isGcd(num1, num2, value){
+  if (num1 % value == 0 && num2 % value == 0 ) {
+    return true;
+  }
+  else {
+    return false;
+  }
+
+
+}
+
+//Exs.12
+
+function ex112() {
+
+
 }
